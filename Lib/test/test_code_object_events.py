d75a6464ca0bfdab4444548c38645ee9fa5f6a09
Jaineel Shah <jaineel@Jaineels-MacBook.local>
2022-10-24 17:57:59 -0400

    Call Back Test

--- Lib/test/test_code_object_events.py
+++ Lib/test/test_code_object_events.py
@@ -6,23 +6,33 @@ from _testcapi import (
     PYCODEOBJECT_EVENT_DESTROYED,
     # restore_code_object_event_callback,
     set_code_object_event_callback,
+    get_code_object_created_counts,
+    get_code_object_destroyed_counts,
 )
 
 class CodeObjectEventsTest(unittest.TestCase):
     def test_code_object_events_dispatched(self):
-        events = []
-        def handle_code_object_event(*args):
-            events.append(args)
-        set_code_object_event_callback(handle_code_object_event)
+        set_code_object_event_callback()
+        created = get_code_object_created_counts()
+        destroyed = get_code_object_destroyed_counts()
 
-        # try:
-        PyCodeObject *mycodeobject
-        self.assertIn((PYCODEOBJECT_EVENT_CREATED, mycodeobject, None), events)
-        mycodeobject_id = id(mycodeobject)
+        if created > 0:
+            print("Code Object Created Couter: ", created)
+        if destroyed > 0:
+            print("Code Object Destroyed Couter: ", destroyed)
+        # events = []
+        # def handle_code_object_event(*args):
+        #     events.append(args)
+        # set_code_object_event_callback(handle_code_object_event)
 
-        # Clear events reference to func
-        events = []
-        del mycodeobject
-        self.assertIn((PYCODEOBJECT_EVENT_DESTROYED, mycodeobject_id, None), events)
+        # # try:
+        # PyCodeObject *mycodeobject
+        # self.assertIn((PYCODEOBJECT_EVENT_CREATED, mycodeobject, None), events)
+        # mycodeobject_id = id(mycodeobject)
+
+        # # Clear events reference to func
+        # events = []
+        # del mycodeobject
+        # self.assertIn((PYCODEOBJECT_EVENT_DESTROYED, mycodeobject_id, None), events)
         # finally:
             # restore_code_object_event_callback()
